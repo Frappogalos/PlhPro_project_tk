882dbbd46aa19dd85d8548ee20615d435902e195
Frappogalos <112326400+Frappogalos@users.noreply.github.com>
2024-04-28 00:05:48 +0300

    Fixed movement bug

--- scripts/cars.py
+++ scripts/cars.py
@@ -4,7 +4,7 @@ import random
 import math
 # Οι θέσεις όπου εμφανίζονται τα αυτοκίνητα όταν δημιουργούνται
 CARS_STARTING_POSITIONS = {"1": [(-40, 530), (-40, 475)], "2": [(730, 950), (800, 950)],
-                           "3": [(1640, 365), (1640, 420)]}
+                           "3": [(1550, 365), (1550, 420)]}
 
 
 class Car:
@@ -43,8 +43,6 @@ class Car:
             return 0, -6
         elif self.direction == 3:
             return -6, 0
-        else:
-            return 0, 6
 
     def move_car(self):
         """Μέθοδος όπου διαχειρίζεται την κίνηση του κάθε αυτοκινήτου"""
@@ -53,22 +51,17 @@ class Car:
             # και αν αυτή είναι κάτω από την οριζόμενη τιμή ακινητοποιείται
             for car in Car.cars_dict[str(self.direction)][self.lane]:
                 if self != car and self.direction == car.direction and self.lane == car.lane:
-                    if self.direction == 1 and car.x - self.x < 120:
+                    if self.direction == 1 and 0 < car.x - self.x < 120:
                         self.moving = False
                         self.speed = (0, 0)
                         self.stopped = car
                         self.root.after(30, self.move_car)
-                    elif self.direction == 3 and self.x - car.x < 120:
+                    elif self.direction == 3 and 0 < self.x - car.x < 120:
                         self.moving = False
                         self.speed = (0, 0)
                         self.stopped = car
                         self.root.after(30, self.move_car)
-                    elif self.direction == 2 and self.y - car.y < 120:
-                        self.moving = False
-                        self.speed = (0, 0)
-                        self.stopped = car
-                        self.root.after(30, self.move_car)
-                    elif self.direction == 4 and car.y - self.y < 120:
+                    elif self.direction == 2 and 0 < self.y - car.y < 120:
                         self.moving = False
                         self.speed = (0, 0)
                         self.stopped = car
@@ -76,14 +69,14 @@ class Car:
         if self.moving:
             # Εφόσον κινείται το αυτοκίνητο αν είναι εντός των ορίων του καμβά συνεχίζει την κίνησή του
             # αλλιώς διαγράφεται το αυτοκίνητο
-            if -100 < self.x < 1700 and -100 < self.y < 1000:
+            if -100 < self.x < 1600 and -100 < self.y < 1000:
                 self.canvas.move(self.car, self.speed[0], self.speed[1])
                 self.x += self.speed[0]
                 self.y += self.speed[1]
                 self.root.after(30, self.move_car)
             else:
                 self.delete_car()
-        if not self.moving:
+        else:
             # Εφόσον είναι σταματημένο το αυτοκίνητο, αν μεγαλώσει η απόσταση από το αυτοκίνητο για το
             # οποίο σταμάτησε ξεκινάει πάλι να κινείται
             if (self.direction == 1 or self.direction == 3) and abs(self.x - self.stopped.x) > 180:
@@ -91,7 +84,7 @@ class Car:
                 self.speed = self.find_speed()
                 self.stopped = None
                 self.root.after(500, self.move_car)
-            elif (self.direction == 2 or self.direction == 4) and abs(self.y - self.stopped.y) > 180:
+            elif self.direction == 2 and abs(self.y - self.stopped.y) > 180:
                 self.moving = True
                 self.speed = self.find_speed()
                 self.stopped = None
