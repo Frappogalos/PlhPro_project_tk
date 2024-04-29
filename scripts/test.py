3ae184011ebcf8b7729af8ae40b9d6107d0b69f7
Frappogalos <112326400+Frappogalos@users.noreply.github.com>
2024-04-29 23:23:14 +0300

    Adding pedestrians

--- scripts/test.py
+++ scripts/test.py
@@ -1,5 +1,5 @@
 from PIL import Image, ImageOps
 
-img = Image.open("../images/pedestrians/Person_1_l.png")
+img = Image.open("../images/pedestrians/Person_2_l.png")
 mirror_img = ImageOps.mirror(img)
-mirror_img.save("../images/pedestrians/Person_1_r.png")
+mirror_img.save("../images/pedestrians/Person_2_r.png")
