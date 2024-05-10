e02c836841fe043dfe9d471359c888833ef92966
Frappogalos <112326400+Frappogalos@users.noreply.github.com>
2024-05-11 01:08:47 +0300

    Added pause button again

--- scripts/buttons.py
+++ scripts/buttons.py
@@ -21,3 +21,21 @@ class OpButton:
         self.button.config(image=self.current_image)
         self.operation = OpButton.modes[OpButton.modes.index(self.operation)-1]
         TrafficLights.initialise(self.operation)
+
+
+class PauseButton:
+    """Κλάση για τη δημιουργία κουμπιού παύσης της προσομοίωσης"""
+    def __init__(self, x, y, image, window):
+        self.posx = x
+        self.posy = y
+        self.image = image
+        self.operation = True
+        self.button = tk.Button(window, image=self.image, command=self.btn_click)
+        self.button.place(x=self.posx, y=self.posy)
+
+    def btn_click(self):
+        """Συνάρτηση για τη λειτουργία του κουμπιού όταν αυτό πατηθεί"""
+        if self.operation:
+            self.operation = False
+        else:
+            self.operation = True
--- scripts/main.py
+++ scripts/main.py
@@ -1,12 +1,15 @@
 import tkinter as tk
-from buttons import OpButton
+from buttons import OpButton, PauseButton
 from PIL import ImageTk, Image
 from cars import Car
 from pedestrians import Pedestrian
 from traffic_lights import TrafficLights
 
-
+# Σχετικές διευθύνσεις που βρίσκονται οι φωτογραφίες
+# του φόντου και του κουμπιού λειτουργίας των
+# φωτεινών σηματοδοτών
 BG_IMAGE = "../images/double_intersection.jpg"
+PAUSE_IMAGE = "../images/buttons/pause_btn.png"
 ON_OFF_RED = "../images/buttons/on_off_red.png"
 ON_OFF_GRN = "../images/buttons/on_off_grn.png"
 
@@ -15,10 +18,9 @@ ON_OFF_GRN = "../images/buttons/on_off_grn.png"
 root = tk.Tk()
 # Τίτλος παραθύρου
 root.title("Traffic Simulator")
-lights_operation = True
 # Φόρτωση εικόνων φόντου και κουμπιών
 bg = ImageTk.PhotoImage(Image.open(BG_IMAGE))
-# pause_img = ImageTk.PhotoImage(Image.open(PAUSE_IMAGE))
+pause_img = ImageTk.PhotoImage(Image.open(PAUSE_IMAGE))
 on_off_red = ImageTk.PhotoImage(Image.open(ON_OFF_RED))
 on_off_green = ImageTk.PhotoImage(Image.open(ON_OFF_GRN))
 
@@ -29,14 +31,16 @@ canvas = tk.Canvas(root, width=bg.width(), height=bg.height())
 canvas.pack(fill="both", expand=True)
 # Ορισμός φόντου στον καμβά
 back_ground = canvas.create_image(bg.width()/2, bg.height()/2, image=bg)
-# Κλήση της συνάρτησης που δημιουργεί τους φωτεινούς σηματοδότες
+# Κλήση της μεθόδου που δημιουργεί τους φωτεινούς σηματοδότες
 TrafficLights.traffic_lights_creator(lights_images=TrafficLights.create_images(), canvas=canvas, root=root)
+# Κλήση της μεθόδου που ξεκινάει τη λειτουργία των φωτεινών σηματοδοτών
 TrafficLights.operation()
 # Κλήση της συνάρτησης που δημιουργεί συνεχώς αυτοκίνητα
 Car.car_creator(images=Car.create_images(), canvas=canvas, root=root)
 # Κλήση της συνάρτησης που δημιουργεί συνεχώς πεζούς
 Pedestrian.pedestrian_creator(ped_images=Pedestrian.create_images(), canvas=canvas, root=root)
-# Δημιουργία των κουμπιών
-OpButton(x=1100, y=50, images=(on_off_green, on_off_red), window=root)
+# Δημιουργία του κουμπιού για την εναλλαγή των λειτουργιών των φωτεινών σηματοδοτών
+PauseButton(x=1200, y=50, image=pause_img, window=root)
+OpButton(x=1300, y=50, images=(on_off_green, on_off_red), window=root)
 
 root.mainloop()
