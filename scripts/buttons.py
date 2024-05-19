23a065adfc61da234bdf341cdfb4745abdc789cd
Frappogalos <112326400+Frappogalos@users.noreply.github.com>
2024-05-19 17:43:47 +0300

    Changed images creation for buttons

--- scripts/buttons.py
+++ scripts/buttons.py
@@ -3,16 +3,20 @@ from traffic_lights import TrafficLights
 from pedestrians import Pedestrian
 from cars import Car
 from lights_controller import LightsController
+from PIL import ImageTk, Image
 
 
 class OpButton:
     """Κλάση για τη δημιουργία κουμπιού λειτουργίας των φαναριών της προσομοίωσης"""
     modes = ["night", "normal"]
+    on_off_red = "../images/buttons/on_off_red.png"
+    on_off_grn = "../images/buttons/on_off_grn.png"
 
-    def __init__(self, x, y, images, window):
+    def __init__(self, x, y, window):
         self.posx = x
         self.posy = y
-        self.images = images
+        self.images = [ImageTk.PhotoImage(Image.open(OpButton.on_off_grn)),
+                       ImageTk.PhotoImage(Image.open(OpButton.on_off_red))]
         self.current_image = self.images[1]
         self.operation = "normal"
         self.button = tk.Button(window, image=self.current_image, command=self.btn_click)
@@ -28,10 +32,12 @@ class OpButton:
 
 class PauseButton:
     """Κλάση για τη δημιουργία κουμπιού παύσης της προσομοίωσης"""
-    def __init__(self, x, y, image, window):
+    pause_image = "../images/buttons/pause_btn.png"
+
+    def __init__(self, x, y, window):
         self.posx = x
         self.posy = y
-        self.image = image
+        self.image = ImageTk.PhotoImage(Image.open(PauseButton.pause_image))
         self.operation = True
         self.button = tk.Button(window, image=self.image, command=self.btn_click)
         self.button.place(x=self.posx, y=self.posy)
--- scripts/main.py
+++ scripts/main.py
@@ -6,13 +6,9 @@ from pedestrians import Pedestrian
 from traffic_lights import TrafficLights
 from lights_controller import LightsController
 
-# Σχετικές διευθύνσεις που βρίσκονται οι φωτογραφίες
-# του φόντου και του κουμπιού λειτουργίας των
-# φωτεινών σηματοδοτών
+# Σχετική διεύθυνση που βρίσκεται η φωτογραφία
+# του φόντου
 BG_IMAGE = "../images/double_intersection.jpg"
-PAUSE_IMAGE = "../images/buttons/pause_btn.png"
-ON_OFF_RED = "../images/buttons/on_off_red.png"
-ON_OFF_GRN = "../images/buttons/on_off_grn.png"
 
 
 # Δημιουργία παραθύρου
@@ -21,9 +17,6 @@ root = tk.Tk()
 root.title("Traffic Simulator")
 # Φόρτωση εικόνων φόντου και κουμπιών
 bg = ImageTk.PhotoImage(Image.open(BG_IMAGE))
-pause_img = ImageTk.PhotoImage(Image.open(PAUSE_IMAGE))
-on_off_red = ImageTk.PhotoImage(Image.open(ON_OFF_RED))
-on_off_green = ImageTk.PhotoImage(Image.open(ON_OFF_GRN))
 
 # Μέγεθος παραθύρου'
 root.geometry(f"{bg.width()}x{bg.height()}")
@@ -41,7 +34,7 @@ Car.car_creator(car_images=Car.create_images(), canvas=canvas, root=root)
 # Κλήση της συνάρτησης που δημιουργεί συνεχώς πεζούς
 Pedestrian.pedestrian_creator(ped_images=Pedestrian.create_images(), canvas=canvas, root=root)
 # Δημιουργία του κουμπιού για την εναλλαγή των λειτουργιών των φωτεινών σηματοδοτών
-PauseButton(x=1200, y=50, image=pause_img, window=root)
-OpButton(x=1300, y=50, images=(on_off_green, on_off_red), window=root)
+PauseButton(x=1200, y=50, window=root)
+OpButton(x=1300, y=50, window=root)
 
 root.mainloop()
